aeed47cc24e2ef70c6100983188f6a159b2641ea
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-01-13 11:29:17 +0000

    total_cash done

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,12 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop)
+  total_cash = pet_shop[:admin][:total_cash]
+  return total_cash
+end
