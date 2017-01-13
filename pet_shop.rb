020c766e629a698f5bf8531a2ac9871439f00f3f
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-01-13 12:01:22 +0000

    add_or_remove_cash done

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,7 +6,6 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop)
-  total_cash = pet_shop[:admin][:total_cash]
-  return total_cash
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
 end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -86,20 +86,20 @@ class TestPetShop < Minitest::Test
 
   def test_add_or_remove_cash_add
     add_or_remove_cash(@pet_shop,10)
-    cash = total_cash(@pet_shop)
-    assert_equal(1010, cash)
+    # cash = total_cash(@pet_shop)
+    assert_equal(1010, total_cash(@pet_shop))
   end
 
-  # def test_add_or_remove_cash__remove
-  #   add_or_remove_cash(@pet_shop,-10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(990, cash)
-  # end
+  def test_add_or_remove_cash__remove
+    add_or_remove_cash(@pet_shop,-10)
+    cash = total_cash(@pet_shop)
+    assert_equal(990, cash)
+  end
 
-  # def test_pets_sold
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(0, sold)
-  # end
+  def test_pets_sold
+    sold = pets_sold(@pet_shop)
+    assert_equal(0, sold)
+  end
 
   # def test_increase_pets_sold
   #   increase_pets_sold(@pet_shop,2)
